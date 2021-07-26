269cc27e850bff6f7ce7b9de231c4a1509d1aa1f
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-07-26 12:47:27 +0200

    :sparkles: : add unicorn-service and add unicorn list page

---
+++ src/app/pages/unicorn-list/unicorn-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unicorn-list',
+  templateUrl: './unicorn-list.component.html',
+  styleUrls: ['./unicorn-list.component.scss']
+})
+export class UnicornListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/services/unicorns.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Unicorn} from "../models/unicorn.model";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnicornsService {
+
+  constructor(private readonly http: HttpClient) {
+  }
+
+  public getUnicorns(): Observable<Unicorn[]> {
+    return this.http.get<Unicorn[]>(`${environment.apiUrl}/unicorns`);
+  }
+
+}
