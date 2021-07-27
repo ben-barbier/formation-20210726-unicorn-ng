c287fff8696f3a53648577fad51a8e77691f9011
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-07-27 15:24:39 +0200

    :sparkles: : add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './layout/nav/nav.component';
 import { UnicornCardComponent } from './pages/unicorn-list/unicorn-card/unicorn-card.component';
 import { UnicornListComponent } from './pages/unicorn-list/unicorn-list.component';
 import { MaterialModule } from './shared/material.module';
 import { AgePipe } from './shared/pipes/age.pipe';
 
 @NgModule({
-    declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, AgePipe],
+    declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, AgePipe, NavComponent],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, MaterialModule],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,17 @@
-import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const matModules = [MatIconModule, MatButtonModule, MatCardModule, LayoutModule, MatToolbarModule, MatSidenavModule, MatListModule];
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, MatIconModule, MatButtonModule, MatCardModule],
-    exports: [MatIconModule, MatButtonModule, MatCardModule],
+    imports: matModules,
+    exports: matModules,
 })
 export class MaterialModule {}
