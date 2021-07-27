f560cbda02ab6c259358552effd8adcce58b1c04
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-07-27 16:57:33 +0200

    :sparkles: : add cart management

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { CartService } from '../../shared/services/cart.service';
 
 @Component({
     selector: 'app-nav',
@@ -9,10 +10,12 @@ import { map, shareReplay } from 'rxjs/operators';
     styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-    isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    public cartCount$ = this.cartService.cart$.pipe(map((cart) => cart.length));
+
+    public isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
         map((result) => result.matches),
         shareReplay()
     );
 
-    constructor(private breakpointObserver: BreakpointObserver) {}
+    constructor(private readonly breakpointObserver: BreakpointObserver, private readonly cartService: CartService) {}
 }
--- src/app/pages/unicorn-list/unicorn-card/unicorn-card.component.ts
+++ src/app/pages/unicorn-list/unicorn-card/unicorn-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { Unicorn } from '../../../shared/models/unicorn.model';
+import { CartService } from '../../../shared/services/cart.service';
 
 @Component({
     selector: 'app-unicorn-card',
@@ -10,7 +12,15 @@ export class UnicornCardComponent {
     @Input() public unicorn?: Unicorn;
     @Output() private deleted = new EventEmitter<void>();
 
+    public isInCart$ = this.cartService.cart$.pipe(map((unicorns) => unicorns.some((u) => u.id === this.unicorn?.id)));
+
+    constructor(private readonly cartService: CartService) {}
+
     public delete(): void {
         this.deleted.emit();
+    }
+
+    public toggle(): void {
+        this.cartService.toggle(this.unicorn!);
     }
 }
